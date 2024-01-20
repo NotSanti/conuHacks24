912c57a6285ea13a4ae1e67fa70a0150f24a79e5
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2024-01-20 16:03:41 -0500

    Add image instead

--- spotSaver/src/Header.tsx
+++ spotSaver/src/Header.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './Header.css'
+import logoImage from '../public/ss_smaller_logo.png'
 
 class Header extends Component {
   render() {
@@ -7,7 +8,8 @@ class Header extends Component {
     return (
       <header className="gradient-bg p-4 header">
         <nav className="flex items-center justify-between">
-          <h1 className="text-white text-lg font-semibold">Spot Saver</h1>
+          {/* <h1 className="text-white text-lg font-semibold">Spot Saver</h1> */}
+          <img src={logoImage} alt="logo" className="logo-img" />
         </nav>
       </header>
     );
