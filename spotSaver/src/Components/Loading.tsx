34cbd6612d2210016e362d5a9256ab00902c4cb4
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2024-01-21 03:42:09 -0500

    Faded loading

--- spotSaver/src/Components/Loading.tsx
+++ spotSaver/src/Components/Loading.tsx
@@ -12,26 +12,33 @@ function Loading() {
   }, []);
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <>
+    <div>
+      {isLoading ? (
+        <img
+        src="/spotsaver_new.png"
+        alt="Loading completed"
+        style={{
+          width: "100%",
+          height: "140%",
+          position: "absolute",
+          top: "-100px",
+          left: "0",
+          transition: "opacity 2s",
+          opacity: fadeOut ? 0 : 1,
+        }}/>
+      ) : (
+        <p></p>
+      )}
+    </div>
+    <div className="overlay">
       {isLoading ? (
         <div className="full-circle-spinner"></div>
       ) : (
-        // <p></p>
-        <img
-          src="/path/to/your/image.jpg"
-          alt="Loading completed"
-          style={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            transition: "opacity 2s",
-            opacity: fadeOut ? 0 : 1,
-          }}
-        />
+        <p></p>
       )}
     </div>
+    </>
   );
 }
 
