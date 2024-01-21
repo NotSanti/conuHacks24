707716f7e7877ccd5d540122c19f940ace9d0b46
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2024-01-21 04:43:05 -0500

    map

--- spotSaver/src/Components/Map.tsx
+++ spotSaver/src/Components/Map.tsx
@@ -64,6 +64,8 @@ function Map() {
 
   console.log(import.meta.env.MAPBOX_KEY);
 
+
+
   const mapSettings = {
     accessToken: import.meta.env.VITE_MAPBOX_ACESS_TOKEN,
     style: "mapbox://styles/mapbox/standard",
